6b8d4691470727f71e9e27b501136bf68efb8d10
Tatyana <tatyana@MacBook-Air-Tatyana.local>
2025-10-20 14:18:36 +0300

    fixed index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // index.js
-import { handler } from './api/gifts.js'; // Импортируем основной обработчик из gifts.js
+import handler from './api/gifts.js'; // Импортируем значение по умолчанию
 
 export default handler; // Экспортируем его как основной обработчик
